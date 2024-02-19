5af3d86ed7cdbd938595db3aa7c909186b13339e
facundo1000 <martinez.facundo85@gmail.com>
2024-02-19 19:26:39 -0300

    Initial commit

---
+++ src/main/java/edu/unam/expedientesapp/ExpedientesAppApplication.java
@@ -0,0 +1,13 @@
+package edu.unam.expedientesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpedientesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpedientesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/unam/expedientesapp/ExpedientesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unam.expedientesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpedientesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
